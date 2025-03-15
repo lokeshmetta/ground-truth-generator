5321e5333a839b7c6b7577d6ba0069895dfdd02e
Lokesh Metta <26522232+lokeshmetta@users.noreply.github.com>
2025-03-15 23:00:18 +0530

    sd

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,16 @@ import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+// If you're using React Router, update your router configuration:
+import { BrowserRouter } from 'react-router-dom';
+
+// Change this:
+<BrowserRouter>
+  <App />
+</BrowserRouter>
+
+// To this:
+<BrowserRouter basename="/ground-truth-generator">
+  <App />
+</BrowserRouter>
